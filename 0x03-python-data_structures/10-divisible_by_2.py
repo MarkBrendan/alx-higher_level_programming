754ff833d4df66f7b4a86415cd9b1b652d9ca53c
MarkBrendan <markobinna65@gmail.com>
2024-01-23 19:53:02 -0800

    task 10

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    my_lit=[]
+    for num in my_list:
+        if num % 2 == 0:
+            my_lit.append(True)
+        else:
+            my_lit.append(False)
+        return my_lit
