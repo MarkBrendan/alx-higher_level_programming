04aa107c6c74c215c739f01eb41beef82350dbd1
MarkBrendan <markobinna65@gmail.com>
2024-02-05 14:44:28 -0800

    task 1

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     ciunt = 0
     for idx in range(x):
         try:
-            print(my_list[idx], end="")
+            print("{}".format(my_list[idx]),  end="")
             ciunt += 1
         except:
             break
