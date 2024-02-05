ccd8e83380f15197934b3482a6ed66cb90c29381
MarkBrendan <markobinna65@gmail.com>
2024-02-05 15:34:34 -0800

    update

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     for idx in range(x):
         try:
             or idx in range(x):
-            print("{}".format(my_list[idx]),  end="")
+            print(my_list[idx], end="")
             ciunt += 1
         except IndexError:
             None
