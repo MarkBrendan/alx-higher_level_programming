561fad240a290503bcf6acada2c125d06706b60c
MarkBrendan <markobinna65@gmail.com>
2024-02-05 14:48:02 -0800

    task 1

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,6 +6,6 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[idx]),  end="")
             ciunt += 1
         except:
-            break
+            pass
     print()
     return ciunt
