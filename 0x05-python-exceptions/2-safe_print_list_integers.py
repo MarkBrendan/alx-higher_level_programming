b7b59a478e0c373907f63cd2a9438ffaeda10fb2
MarkBrendan <markobinna65@gmail.com>
2024-02-07 03:01:28 -0800

    task 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,9 +3,10 @@ def safe_print_list_integers(my_list=[], x=0):
     ciunt = 0
     for idx in range(x):
         try:
+            int(my_list[idx])
             print("{:d}".format(my_list[idx]), end="")
             ciunt += 1
         except IndexError:
-            None
+            continue
     print()
     return ciunt
