7a57a55df870130ddeaceb55f3ceb1f9c0d2b3cd
MarkBrendan <markobinna65@gmail.com>
2024-02-07 13:17:05 -0800

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""An empty Square class"""
+
+
+class Square:
+    """A class Square"""
+
+    def __init__(self, size=None):
+        """Initialize a new square.
+        Args:
+        size (int): The size of the new square.
+        """
+        self.__size = size
