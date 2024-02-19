bcd44bb9f49e2f3234bde1e71695b51705a25e78
MarkBrendan <markobinna65@gmail.com>
2024-02-19 08:04:42 -0800

    tack 0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
+"""returns the list of available attributes and methods of an object"""
+
+
 def lookup(obj):
+    """Return the list of attribute and methon"""
+
     return dir(obj)
