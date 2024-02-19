30eae85870508bf4301b92039cc307fad8dee5aa
MarkBrendan <markobinna65@gmail.com>
2024-02-19 10:51:48 -0800

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Define a class BaseGeometry"""
+
+
+class BaseGeometry:
+
+    def area(self):
+        """define the method"""
+        raise Exception("area() is not implemented")
