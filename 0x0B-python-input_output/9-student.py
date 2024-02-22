dfe9172b2a50bb33be1e5935adca4d93ac0f0d41
MarkBrendan <markobinna65@gmail.com>
2024-02-22 12:22:04 -0800

    task 9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -6,7 +6,7 @@ class Student:
     """Defines a student"""
     def __init__(self, first_name, last_name, age):
         """initialise the object"""
-        self.first_nane = first_name
+        self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
