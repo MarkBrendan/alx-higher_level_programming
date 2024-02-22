76209aca060ba32b2a55239a7690cb90bb620e7e
MarkBrendan <markobinna65@gmail.com>
2024-02-22 07:01:17 -0800

    task 3

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+"""module that gives the JSON representation of an object"""
+
 import json
+
 
 def to_json_string(my_obj):
     """Define a function that gives the JSON representation of an obj"""
