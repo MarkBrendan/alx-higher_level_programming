259213d26478b7365e40c211b6e678f104ed22a8
MarkBrendan <markobinna65@gmail.com>
2024-02-21 16:23:45 -0800

    task update

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,5 +3,5 @@
 def read_file(filename=""):
     """Define a function the read text from a file"""
 
-    with open("my_file_0.txt", encoding='utf-8') as flin:
+    with open(filename, encoding='utf-8') as flin:
         print(flin.read(), end=" ")
