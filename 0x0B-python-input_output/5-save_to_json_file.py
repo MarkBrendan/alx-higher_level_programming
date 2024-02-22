d6f7a1264202b8eebd0de81f1d32bd0f86c77c62
MarkBrendan <markobinna65@gmail.com>
2024-02-22 07:44:09 -0800

    task 5

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""A function that writes an Object to a text file, using a JSON representation"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Define a function that returns an object represented by a JSON string"""
+    with open(filename, 'w') as f:
+        json.dump(my_obj, f)
