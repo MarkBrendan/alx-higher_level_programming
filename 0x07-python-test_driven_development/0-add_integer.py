274aa9def475b9c74857052f97d8452729ff5a40
MarkBrendan <markobinna65@gmail.com>
2024-04-07 06:38:08 -0700

    task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,12 +1,23 @@
 #!/usr/bin/python3
+"""A module that adds two number and return the value"""
+
+
 def add_integer(a, b=98):
-    """Returns an integer: the addition of a and b
+    """add_integer function add of a and b
     a and b must be integers or floats
+
     Raise:
         TypeError: a must be an integer or b must be an integer
-        """
-    if not isinstance(a, int) or not isinstance(b, int):
-        raise TypeError(a must be an integer)
-    elif not isinstance(b, (int, float)):
-        raise TypeError(b must be an integer)
+
+        Args:
+            a: first integer
+            b: second integer with a place holder of 98
+
+        Return:
+            return the value. a + b
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
     return (int(a) + int(b))
