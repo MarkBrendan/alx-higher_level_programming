72ebfe49e93ab4c84ad2cc49f7cc2bcd1b61cc1e
MarkBrendan <markobinna65@gmail.com>
2024-04-15 16:23:00 -0700

    task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -15,7 +15,6 @@ def inherits_from(obj, a_class):
                 of the specified class ; otherwise False.
     """
 
-    
     if type(obj) is a_class:
         return False
     return issubclass(type(obj), a_class)
