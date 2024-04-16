9a09ef0bf5d3499331443fb4877b3325f64913b9
MarkBrendan <markobinna65@gmail.com>
2024-04-15 17:33:04 -0700

    task 8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""A class BaseGeometry"""
+
+
+class BaseGeometry:
+    """an class BaseGeometry"""
+
+    def area(self):
+        """Define a area method"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Define a integer_validator that validate integer"""
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value < 0 or value == 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+        self.name = name
+        self.value = value
+
+class Rectangle(BaseGeometry):
+    """An inherited call Rectangle"""
+
+    def __init__(self, width, height):
+        """initialise a constructor"""
+        self.__width = 0
+        self.__height = 0
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
