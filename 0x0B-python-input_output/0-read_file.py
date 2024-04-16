c9794f70412277caf94a00512cbe845406eceec3
MarkBrendan <markobinna65@gmail.com>
2024-04-16 16:58:54 -0700

    task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""A 0-read_file.py module"""
+
+
+def read_file(filename=""):
+    """a function that reads a text file (UTF8) and prints it to stdout
+
+    Args:
+        filename: first parameter
+
+
+    Print:
+        prints it to stdout.
+    """
+
+    with open(filename, "r", encoding="utf-8") as f:
+        for lin in f:
+            print(lin)
