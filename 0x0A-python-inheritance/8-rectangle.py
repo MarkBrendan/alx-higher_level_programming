e927fa535383760c3e46613a702eb814251d7754
MarkBrendan <markobinna65@gmail.com>
2024-04-15 17:45:00 -0700

    task 8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -13,7 +13,7 @@ class BaseGeometry:
         """Define a integer_validator that validate integer"""
         if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
-        if value < 0 or value == 0:
+        if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
 
         self.name = name
