6f4d93426542b63d9e4935e4d3c4580030756a33
MarkBrendan <markobinna65@gmail.com>
2024-04-21 16:02:40 +0100

    task 1-3

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/puthon3
+"""base.py module"""
+
+
+class Base:
+    """A class Base"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
