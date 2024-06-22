ade77986cd969debe3fd0e3558e91f089d72dacd
MarkBrendan <markobinna65@gmail.com>
2024-06-22 02:08:00 +0100

    task 2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,11 +2,12 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
-      return {};
-    }
-    this.width = w;
-    this.height = h;
+    if (w <= 0 || h <= 0 || typeof w !== 'number'|| typeof h !== 'number') {
+        return {};
+    } else {
+        this.width = w;
+        this.height = h;
+      }
   }
 }
 
