78671be7f89a95fbe1aba4a64491167ff04be29f
MarkBrendan <markobinna65@gmail.com>
2024-06-27 16:42:32 -0700

    1. Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""1-filter_states A script that lists all states with a name
-starting with N"""
+"""1-filter_states A script that lists all states with a name starting with N"""
 
 import MySQLdb
 import sys
