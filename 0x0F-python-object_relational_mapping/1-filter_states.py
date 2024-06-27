9b108425033414b8353c7de4e527bb862b84d6c3
MarkBrendan <markobinna65@gmail.com>
2024-06-27 16:40:05 -0700

    1. Filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""1-filter_states A script that lists all states with a name
+starting with N"""
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+
+    db = MySQLdb.connect(
+            host="localhost", port=3306, user=sys.argv[1],
+            password=sys.argv[2], database=sys.argv[3])
+
+    cont = db.cursor()
+    cont.execute("SELECT * FROM states
+                 WHERE name LIKE 'N%' ORDER BY states.id ASC")
+    get_all = cont.fetchall()
+    for i in get_all:
+        print(i)
+    cont.close()
+    db.close()
