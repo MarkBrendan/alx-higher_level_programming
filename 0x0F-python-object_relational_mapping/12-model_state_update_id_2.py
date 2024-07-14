094adc887dacea8f05ff4d713eea6d99a4d80839
MarkBrendan <markobinna65@gmail.com>
2024-07-14 04:34:29 -0700

    task 12

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Write a script that changes the name of a State
+object from the database hbtn_0e_6_usa"""
+
+
+from model_state import Base, State
+from sys import argv
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+if __name__ == '__main__':
+    """Create a connection and a session to interacte with the database"""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format
+                           (argv[1], argv[2], argv[3]), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(engine)
+    session = Session()
+
+    """update the name"""
+    session.query(State).filter(State.id == 2).update({'name': 'New Mexico'})
+
+
+    """close the session"""
+    session.close()
