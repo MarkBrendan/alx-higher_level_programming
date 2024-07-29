ae68310613db5608958eb11a5f400fa1e34856e2
MarkBrendan <markobinna65@gmail.com>
2024-07-29 15:38:17 -0700

    update

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Write a Bash script that takes in a URL and displays all HTTP methods the server will accept.
-curl -sX "OPTIONS" -i $1 | grep -i allow | cut -d ' ' -f 2- 
+curl -sX "OPTIONS" -I $1 | grep -i allow | cut -d ' ' -f 2- 
