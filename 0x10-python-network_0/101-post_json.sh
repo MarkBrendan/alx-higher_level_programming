b87b4d60022f84af5b5f1450a1368f84f3500442
MarkBrendan <markobinna65@gmail.com>
2024-07-30 06:20:35 -0700

    update

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Write a Bash script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
-curl -s --json @my_json_0 $1
+curl -s -d @my_json_0 $1
