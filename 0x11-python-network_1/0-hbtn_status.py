723cc3b9dae55da2f76de3116e44138737d98538
MarkBrendan <markobinna65@gmail.com>
2024-07-30 17:31:29 -0700

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
+"""0-hbtn_status.py module"""
+
 
 import urllib.request
 
 
 if __name__ == '__main__':
-    
+
+    """request the page and open it with urlopen"""
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as r:
+
+        """read the page and print out the content"""
         body = r.read()
         print("Body response:")
         print("\t - type: {}\n \t - content: {}\n \t - utf8 content: {}"
-                .format(type(body), body, body))
+              .format(type(body), body, body))
