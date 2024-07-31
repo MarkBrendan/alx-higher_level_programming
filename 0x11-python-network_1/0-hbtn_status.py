b535818e135d8f78c80e03e7911241b54decb398
MarkBrendan <markobinna65@gmail.com>
2024-07-30 17:49:40 -0700

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,9 +8,11 @@ import urllib.request
 if __name__ == '__main__':
 
     """request the page and open it with urlopen"""
+
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as r:
 
         """read the page and print out the content"""
+
         body = r.read()
         print("Body response: \n\t - type: {}\n\t - content: {}\n\t - utf8
               content: {}".format(type(body), body, body.decode('utf-8')))
