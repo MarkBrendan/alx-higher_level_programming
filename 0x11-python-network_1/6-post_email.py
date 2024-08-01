dff92510b1746a1451c44d464b76a4c89e24db56
MarkBrendan <markobinna65@gmail.com>
2024-08-01 16:59:15 -0700

    task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+a python script that takes in a URL and an email, sends a POST request
+And passed URL with the email as a parameter,
+and finally displays the body of the response.
+"""
+
+if __name__ == '__main__':
+
+    import requests
+    import sys
+
+    url = sys.argv[1]
+    mail = sys.argv[2]
+
+    data = requets.post(url, email=mail)
+        print("{}".format(data.text)
